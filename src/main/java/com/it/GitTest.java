b089c5d0cb370e20ad3f6a319b15251882a22911
fengyetong <136207616+fengyetong@users.noreply.github.com>
2023-06-12 10:37:18 +0800

    git commit8

--- src/main/java/com/it/GitTest.java
+++ src/main/java/com/it/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello,git5!");
         System.out.println("push,git5!");
         System.out.println("git pull,git6!");
+        System.out.println("git test,git8!");
     }
 }
